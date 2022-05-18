981e1021f6010a04e855cad4af68165c6a10f643
Jeffrey Zhao <jeffreyzhao2003@gmail.com>
2022-05-17 23:45:58 -0400

    Added login page (needs to implement login)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Flex
+} from '@chakra-ui/react';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex w='100vw' height='100vh' justifyContent='center' alignItems='center'>
+      <Login />
+    </Flex>
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,48 @@
+import {
+    VStack,
+    Input,
+    Button,
+    FormControl,
+    FormLabel,
+    Heading,
+    Text,
+    useBreakpointValue
+} from '@chakra-ui/react';
+import {FcGoogle} from 'react-icons/fc';
+
+const login = () => {
+    console.log('Logged in')
+}
+
+const Login = () => {
+    return(
+        <VStack width='30%' spacing={8}
+        py={{ base: '0', sm: '8' }}
+        px={{ base: '4', sm: '10' }}
+        boxShadow={{ base: 'none', sm: 'md'}}
+        borderRadius={{ base: 'none', sm: 'xl' }}>
+            <Heading as='h1' color='red'> <Text as='span' color='blue'>NBA</Text> Talks</Heading>
+            <FormControl>
+                <FormLabel htmlFor='email'>Email</FormLabel>
+                <Input id='email' type='email'></Input>
+            </FormControl>
+            <FormControl>
+                <FormLabel htmlFor='password'>Password</FormLabel>
+                <Input id='password' type='password'></Input>
+            </FormControl>
+            <Button colorScheme='blue' w='100%' onClick={login}>Login</Button>
+            <Button                  
+                variant='outline' 
+                size='lg' 
+                fontSize='lg' 
+                w='100%' 
+                leftIcon={<FcGoogle />}
+                aria-label={'Google'}
+            >
+                Continue with Google
+            </Button>
+        </VStack>
+    )
+}
+
+export default Login;
